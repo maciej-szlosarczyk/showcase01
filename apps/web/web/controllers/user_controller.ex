152c1812b1ea9bbdb4821573d20abc9061dbbd74
Maciej Szlosarczyk <maciej.szlosarczyk@inbank.ee>
2016-09-18 13:36:43 +0300

    Add first attempt at decoupling logic from web

--- apps/web/web/controllers/user_controller.ex
+++ apps/web/web/controllers/user_controller.ex
@@ -1,11 +1,11 @@
 defmodule Web.UserController do
-@moduledoc false
+  @moduledoc false
 
-use Web.Web, :controller
-alias Domain.Users
+  use Web.Web, :controller
+  alias Domain.Users
 
-def index(conn, _params) do
-  users = Users.all
-  render(conn, "index.html", users: users)
-end
+  def index(conn, _params) do
+    users = Users.all
+    render(conn, "index.html", users: users)
+  end
 end
