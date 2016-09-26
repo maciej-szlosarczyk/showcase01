c3a43c7e4f6f849dc30d710576c1da17a1a12fd8
Maciej Szlosarczyk <maciej.szlosarczyk@inbank.ee>
2016-09-26 22:25:34 +0300

    Add order migration

---
+++ apps/domain/priv/repo/migrations/20160926192147_add_order_table.exs
@@ -0,0 +1,18 @@
+defmodule Domain.Repo.Migrations.AddOrderTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:orders) do
+      add :base_currency, :string, null: false
+      add :target_currency, :string, null: false
+      add :amount, :decimal, null: false
+      add :start_date, :date, null: false
+      add :end_date, :date, null: false
+      add :user_id, references(:users, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+    create index(:orders, [:user_id])
+  end
+end
