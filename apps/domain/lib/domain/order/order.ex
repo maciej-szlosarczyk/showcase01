3588c01439ad06affbd836c3a34c6c8b0ab24700
Maciej Szlosarczyk <maciej.szlosarczyk@inbank.ee>
2016-09-28 20:06:42 +0300

    Normalize currency choices inside order

--- apps/domain/lib/domain/order/order.ex
+++ apps/domain/lib/domain/order/order.ex
@@ -1,10 +1,14 @@
 defmodule Domain.Order do
   @moduledoc false
 
-  @currency_choices ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK","SGD", "THB", "TRY", "USD", "ZAR"]
+  @currency_choices ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK",
+                     "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR",
+                     "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN",
+                     "RON", "RUB", "SEK","SGD", "THB", "TRY", "USD", "ZAR"]
 
   use Ecto.Schema
-  import Ecto.Changeset
+  alias Ecto.Date
+ import Ecto.Changeset
 
   schema "orders" do
     field :base_currency, :string, null: false
@@ -28,4 +32,21 @@ defmodule Domain.Order do
     |> validate_inclusion(:target_currency, currency_choices)
     |> validate_required([:base_currency, :target_currency, :amount, :end_date])
   end
+
+  def update_changeset(order, params \\ %{}) do
+    order
+    |> cast(params, [:base_currency, :target_currency, :amount, :end_date])
+    |> validate_inclusion(:base_currency, currency_choices)
+    |> validate_inclusion(:base_currency, currency_choices)
+  end
+
+  def set_start_date(order) do
+    case order.start_date do
+      nil ->
+        order
+        |> put_change(:start_date, Date.cast(Date.utc))
+      _   ->
+        order
+    end
+  end 
 end
